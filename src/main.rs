eb63a6edc18d1106ae3875eded5d532c070b7a7a
aza547 <alexander-12345@hotmail.co.uk>
2024-07-21 09:50:03 +0100

    fix beta/ptr for retail

--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,19 @@ fn main() {
     let mut system = System::new_all();
 
     loop {
-        // Refresh all system information
         system.refresh_all();
 
-        // Get list of processes
-        let processes = system.processes();
+        let mut retail_processes = system.processes_by_exact_name("Wow.exe");
+        let mut retail_beta_processes = system.processes_by_exact_name("WowB.exe");
+        let mut retail_ptr_processes = system.processes_by_exact_name("WowT.exe");
+        let mut classic_processes = system.processes_by_exact_name("WowClassic.exe");
 
-        // Print process information
-        println!("Running Processes:");
-        for (pid, process) in processes {
-            println!("PID: {}, Name: {}", pid, process.name());
-        }
+        let retail = retail_processes.next().is_some() || retail_beta_processes.next().is_some() || retail_ptr_processes.next().is_some();
+        let classic = classic_processes.next().is_some();
 
-        // Sleep for 1 second
+        // This is JSON.
+        println!("{{ \"Retail\": {}, \"Classic\": {} }}", retail, classic);
+
         thread::sleep(time::Duration::from_secs(2));
     }
 }

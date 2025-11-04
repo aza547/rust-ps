d026350a2c6af080ce29a1ee8619992bf7f044f5
aza547 <alexander-12345@hotmail.co.uk>
2025-11-04 20:35:39 +0000

    add classic ptr

--- src/main.rs
+++ src/main.rs
@@ -12,9 +12,10 @@ fn main() {
         let mut retail_beta_processes = system.processes_by_exact_name(OsStr::new("WowB.exe"));
         let mut retail_ptr_processes = system.processes_by_exact_name(OsStr::new("WowT.exe"));
         let mut classic_processes = system.processes_by_exact_name(OsStr::new("WowClassic.exe"));
+        let mut classic_ptr_processes = system.processes_by_exact_name(OsStr::new("WowClassicT.exe"));
 
         let retail = retail_processes.next().is_some() || retail_beta_processes.next().is_some() || retail_ptr_processes.next().is_some();
-        let classic = classic_processes.next().is_some();
+        let classic = classic_processes.next().is_some() || classic_ptr_processes.next().is_some();
 
         // This is JSON.
         println!("{{ \"Retail\": {}, \"Classic\": {} }}", retail, classic);
